542d32535398cfa6dcf3d26ea624253c87f87179
rishabh anand <rish@rishabhs-MacBook-Pro.local>
2022-05-18 21:56:20 +0800

    Add GCN and GIN

--- models/models.py
+++ models/models.py
@@ -15,3 +15,30 @@ servings = {
     "k-GNN": None, # k-WL graph neural network
     "MPNN": None # message passing neural network
 }
+
+class GCN(nn.Module):
+    def __init__(self, indim, hidden, outdim):
+        self.conv1 = tgnn.GraphConv(indim, hidden)
+        self.conv2 = tgnn.GraphConv(hidden, hidden)
+        self.conv3 = tgnn.GraphConv(hidden, outdim)
+
+    def forward(self, x, edge_idx):
+        x = torch.relu(self.conv1(x, edge_idx))
+        x = torch.relu(self.conv2(x, edge_idx))
+        out = torch.softmax(self.conv3(x, edge_idx), 1)
+
+        return out
+
+class GIN(nn.Module):
+    def __init__(self, indim, hidden, outdim):
+        self.l1 = nn.Linear()
+        self.conv1 = tgnn.GINConv(nn, )
+        self.conv2 = tgnn.GraphConv(hidden, hidden)
+        self.conv3 = tgnn.GraphConv(hidden, outdim)
+
+    def forward(self, x, edge_idx):
+        x = torch.relu(self.conv1(x, edge_idx))
+        x = torch.relu(self.conv2(x, edge_idx))
+        out = torch.softmax(self.conv3(x, edge_idx), 1)
+
+        return out        
